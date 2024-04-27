c54e949ca4f75e78e777c4bed76b3792fcaa832c
Sanjay Sargam <sargamsanjaykumar@gmail.com>
2024-04-27 13:46:15 +0530

    api

---
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/custom_info/DataModal.kt
@@ -0,0 +1,18 @@
+package com.fingerprintjs.android.fingerprint.custom_info
+
+import com.google.gson.annotations.SerializedName
+
+public data class DataModal (
+    @SerializedName("drona_android_payload") val payload: String?,
+)
+
+public data class ApiResponse (
+    @SerializedName("message") val message: String?,
+    @SerializedName("data") val data: Data?,
+)
+
+public data class Data(
+    @SerializedName("_id") val id: String?,
+    @SerializedName("drona_android_payload") val payload: String?,
+    @SerializedName("__v") val v: Int?
+)
--- fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/custom_info/DeviceInfoItem.kt
+++ fingerprint/src/main/java/com/fingerprintjs/android/fingerprint/custom_info/DeviceInfoItem.kt
@@ -1,4 +1,4 @@
-package com.fingerprintjs.android.fingerprint.other_info
+package com.fingerprintjs.android.fingerprint.custom_info
 
 public data class DeviceInfoItem(
     val title: String,
